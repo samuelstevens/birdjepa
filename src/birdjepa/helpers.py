f468a81d89ec4f5293428e16c276be9afac592c7
Samuel Stevens <samuel.robert.stevens@gmail.com>
2025-12-30 14:40:06 -0500

    add updated version of progress with total arg

--- src/birdjepa/helpers.py
+++ src/birdjepa/helpers.py
@@ -51,33 +51,63 @@ def setup_sharding(
     return mesh, data_sharding, model_sharding
 
 
+@beartype.beartype
 class progress:
-    """Wraps an iterable with logging instead of tqdm (works better with log files)."""
+    """
+    Wraps an iterable with a logger like tqdm but doesn't use any control codes to manipulate a progress bar, which doesn't work well when your output is redirected to a file. Instead, simple logging statements are used, but it includes quality-of-life features like iteration speed and predicted time to finish.
 
-    def __init__(self, it, *, every: int = 10, desc: str = "progress"):
+    Args:
+        it: Iterable to wrap.
+        every: How many iterations between logging progress.
+        desc: What to name the logger.
+        total: If non-zero, how long the iterable is.
+    """
+
+    def __init__(
+        self,
+        it: collections.abc.Iterable,
+        *,
+        every: int = 10,
+        desc: str = "progress",
+        total: int = 0,
+    ):
         self.it = it
-        self.every = every
+        self.every = max(every, 1)
         self.logger = logging.getLogger(desc)
+        self.total = total
 
     def __iter__(self):
         start = time.time()
+
+        try:
+            total = len(self)
+        except TypeError:
+            total = None
+
         for i, obj in enumerate(self.it):
             yield obj
+
             if (i + 1) % self.every == 0:
-                elapsed = time.time() - start
-                rate = (i + 1) / (elapsed / 60)
-                if isinstance(self.it, collections.abc.Sized):
-                    remaining = (len(self) - (i + 1)) / rate
+                now = time.time()
+                duration_s = now - start
+                per_min = (i + 1) / (duration_s / 60)
+
+                if total is not None:
+                    pred_min = (total - (i + 1)) / per_min
                     self.logger.info(
-                        "%d/%d (%.1f%%) | %.1f it/m | ~%.1fm left",
+                        "%d/%d (%.1f%%) | %.1f it/m (expected finish in %.1fm)",
                         i + 1,
-                        len(self),
-                        (i + 1) / len(self) * 100,
-                        rate,
-                        remaining,
+                        total,
+                        (i + 1) / total * 100,
+                        per_min,
+                        pred_min,
                     )
                 else:
-                    self.logger.info("%d/? | %.1f it/m", i + 1, rate)
+                    self.logger.info("%d/? | %.1f it/m", i + 1, per_min)
 
     def __len__(self) -> int:
+        if self.total > 0:
+            return self.total
+
+        # Will throw exception.
         return len(self.it)
